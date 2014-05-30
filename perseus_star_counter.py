eb3d4a1473924d2b315c39dd8150639dc62adbe8
Tom Rice <t.rice90@gmail.com>
2014-05-30 01:07:00 -1000

    Made perseus star counter awesome.

--- perseus_star_counter.py
+++ perseus_star_counter.py
@@ -25,7 +25,7 @@ import numpy as np
 
 import atpy
 
-import periodic_selector as ps
+import long_periods as ps
 
 def c_print(str):
     if __name__ == '__main__':
@@ -34,17 +34,13 @@ def c_print(str):
         pass
     return
 
-dropbox_bo_data = os.path.expanduser("~/Dropbox/Bo_Tom/data/")
-    
+dropbox_bo_data = os.path.expanduser("~/Dropbox/Bo_Tom/NGC1333/WSERV7/DATA/")
+dropbox_bo_spread = dropbox_bo_data+'spreadsheet/'
 
 #spread = atpy.Table("/home/tom/reu/ORION/DATA/fdece_graded_clipped0.8_scrubbed0.1_dusted0.5_spread.fits")
-spread = atpy.Table(dropbox_bo_data+"fdece_graded_clipped0.8_scrubbed0.1_dusted0.5_spread_pstar.fits")
+spread = atpy.Table(dropbox_bo_spread+"fdece_graded_clipped0.95_scrubbed0.1_dusted0.5_spread.fits")
 
-maxvars_spread_per = atpy.Table(dropbox_bo_data+"maxvars_data_statsper.fits")
-maxvars_s1_spread_per = atpy.Table(dropbox_bo_data+"maxvars_data_s1_statsper.fits")
-
-maxvars_pstar = atpy.Table(dropbox_bo_data+"maxvars_pstar.fits")
-
+maxvars_spread_per = atpy.Table(dropbox_bo_spread+"low_maxvars_photometry_aboveStetson0.5_spread.fits")
 
 # Number of detected sources in the dataset
 c_print( "Number of detected sources in the dataset:" )
@@ -66,7 +62,7 @@ c_print( len(minimum) )
 # Automatic variables
 # Criteria:
 #  At least one band must meet all of the following:
-#  -At least 50 detections, less than 125
+#  -At least 50 detections, less than 200
 #  -Brightness between limits (11<K<16, 11<H<16, 11<J<17)
 #  -No error flags at all (i.e. N_"info" = 0; severe stuff gets clipped anyway) 
 #  AND Stetson value >= 1.
@@ -75,35 +71,11 @@ c_print( len(minimum) )
 
 sp = spread
 
-maxvars = sp.where( (sp.Stetson > 1) & (
+maxvars = sp.where( (sp.Stetson > 0.5) & (
         (sp.N_j >= 50) |
         (sp.N_k >= 50) |
         (sp.N_h >= 50) ) )
-c_print( "Maximum possible number of variables: %d" % len(maxvars) )
-
-
-autovars_old = sp.where( 
-    (sp.Stetson > 1) & ( (
-        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
-        (sp.j_mean > 11) & (sp.j_mean < 17) & # J
-        (sp.N_j_info == 0) ) | (              # J
-        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-        (sp.h_mean > 11) & (sp.h_mean < 16) & # H
-        (sp.N_h_info == 0) ) | (              # H
-        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
-        (sp.k_mean > 11) & (sp.k_mean < 16) & # K
-        (sp.N_k_info == 0) ) ) )              # K
-
-autocandidates_old = sp.where( (
-        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
-        (sp.j_mean > 11) & (sp.j_mean < 17) & # J
-        (sp.N_j_info == 0) ) | (              # J
-        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-        (sp.h_mean > 11) & (sp.h_mean < 16) & # H
-        (sp.N_h_info == 0) ) | (              # H
-        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
-        (sp.k_mean > 11) & (sp.k_mean < 16) & # K
-        (sp.N_k_info == 0) ) )                # K
+c_print( "Maximum possible number of variables (Stetson > 0.5) : %d" % len(maxvars) )
 
 # "True" variability criterion has two cases:
 # 1. All 3 bands are quality, and S > 1 (this is identical to CygOB7), or
@@ -115,17 +87,17 @@ autocandidates_old = sp.where( (
 # Case 1: all 3 bands are quality; S > 1. Note "&"s uniform throughout.
 case1 = ( (sp.Stetson > 1) & (sp.pstar_median > 0.75) &
           (
-        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
+        (sp.N_j >= 50) & (sp.N_j <= 200) &    # J band criteria
         (sp.j_mean > 11) & (sp.j_mean < 17) & 
         (sp.N_j_info == 0) 
         ) &
           (
-        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-        (sp.h_mean > 11) & (sp.h_mean < 16) & 
+        (sp.N_h >= 50) & (sp.N_h <= 200) &    # H band criteria
+        (sp.h_mean > 11) & (sp.h_mean < 16.7) & 
         (sp.N_h_info == 0) 
         ) &
           (
-        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
+        (sp.N_k >= 50) & (sp.N_k <= 200) &    # K band criteria
         (sp.k_mean > 11) & (sp.k_mean < 16) & 
         (sp.N_k_info == 0)
         ) )
@@ -135,28 +107,24 @@ case1 = ( (sp.Stetson > 1) & (sp.pstar_median > 0.75) &
 # criteria.
 case2 = ( ((sp.Stetson > 1) & (sp.pstar_median > 0.75)) & (
           (
-        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
+        (sp.N_j >= 50) & (sp.N_j <= 200) &    # J band criteria
         (sp.j_mean > 11) & (sp.j_mean < 17) & 
         (sp.N_j_info == 0) & (sp.j_rchi2 > 1) 
         ) |
           (
-        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-        (sp.h_mean > 11) & (sp.h_mean < 16) & 
+        (sp.N_h >= 50) & (sp.N_h <= 200) &    # H band criteria
+        (sp.h_mean > 11) & (sp.h_mean < 16.7) & 
         (sp.N_h_info == 0) & (sp.h_rchi2 > 1) 
         ) |
           (
-        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
+        (sp.N_k >= 50) & (sp.N_k <= 200) &    # K band criteria
         (sp.k_mean > 11) & (sp.k_mean < 16) & 
         (sp.N_k_info == 0) & (sp.k_rchi2 > 1) 
         ) ) )
 
-# Adding a location-based conditional to autovars, since there were issues
-# with stars on the easternmost edge of the field.
-case3 = np.degrees(sp.RA) <= 84.2514
+autovars_true = sp.where( case1 | case2 )
 
-autovars_true = sp.where( (case1 | case2) & case3 )
-
-autovars_strict = sp.where( case1 & case3)
+autovars_strict = sp.where( case1 )
 
 
 # Now, to count how many stars have quality that meets "autovars_true".
@@ -164,17 +132,17 @@ autovars_strict = sp.where( case1 & case3)
 # Constructing these as two separate arrays for ease of reading/editing.
 # Case 1: all 3 bands are quality. Note "&"s uniform throughout.
 cand_case1 = ( (sp.pstar_median > 0.75) & ( 
-        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
+        (sp.N_j >= 50) & (sp.N_j <= 200) &    # J band criteria
         (sp.j_mean > 11) & (sp.j_mean < 17) & 
         (sp.N_j_info == 0) 
         ) &
           (
-        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-        (sp.h_mean > 11) & (sp.h_mean < 16) & 
+        (sp.N_h >= 50) & (sp.N_h <= 200) &    # H band criteria
+        (sp.h_mean > 11) & (sp.h_mean < 16.7) & 
         (sp.N_h_info == 0) 
         ) &
           (
-        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
+        (sp.N_k >= 50) & (sp.N_k <= 200) &    # K band criteria
         (sp.k_mean > 11) & (sp.k_mean < 16) & 
         (sp.N_k_info == 0)
         ) )
@@ -183,39 +151,33 @@ cand_case1 = ( (sp.pstar_median > 0.75) & (
 # as well as another layer of parentheses around the complex of "|" criteria.
 cand_case2 = ( (sp.pstar_median > 0.75) & (
     (
-        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
+        (sp.N_j >= 50) & (sp.N_j <= 200) &    # J band criteria
         (sp.j_mean > 11) & (sp.j_mean < 17) & 
         (sp.N_j_info == 0) 
         ) |
     (
-        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-        (sp.h_mean > 11) & (sp.h_mean < 16) & 
+        (sp.N_h >= 50) & (sp.N_h <= 200) &    # H band criteria
+        (sp.h_mean > 11) & (sp.h_mean < 16.7) & 
         (sp.N_h_info == 0) 
         ) |
     (
-        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
+        (sp.N_k >= 50) & (sp.N_k <= 200) &    # K band criteria
         (sp.k_mean > 11) & (sp.k_mean < 16) & 
         (sp.N_k_info == 0) 
         ) ) )
 
 
-autocan_true = sp.where( (cand_case1 | cand_case2) & case3 )
+autocan_true = sp.where( cand_case1 | cand_case2 )
 
-autocan_strict = sp.where( cand_case1 & case3)
+autocan_strict = sp.where( cand_case1 )
 
 c_print( "Number of stars automatically classed as variables: %d" % len(autovars_true) )
 c_print( "Number of stars that have the data quality for auto-classification: %d" % len(autocan_true) )
 
 subjectives = maxvars.where( ~np.in1d(maxvars.SOURCEID, autovars_true.SOURCEID))
 
-old_subjectives = atpy.Table(dropbox_bo_data+"old_subjectives.fits")
-
-new_subjectives = subjectives.where( ~np.in1d(subjectives.SOURCEID, old_subjectives.SOURCEID))
-
 c_print( "" )
 c_print( "Number of probably-variable stars requiring subjective verification due to imperfect data quality: %d" % len(subjectives) )
-
-c_print( "Number of new subjectives: %d" % len(new_subjectives) )
 
 # Now let's count stars that meet our strict criteria in ALL 3 bands
 
@@ -230,27 +192,21 @@ c_print( " Q: Statistically, what fraction of our stars are variables?" )
 c_print( " A: %.2f%s, drawn from the tightest-controlled sample;" % (len(autovars_strict)/len(autocan_strict) * 100, r"%") )
 c_print( "    %.2f%s, drawn from a looser sample." % (len(autovars_true)/len(autocan_true) * 100, r"%") )
 
-# Now for periodicity analysis, which relies on periodic_selector
+# Now for periodicity analysis, which relies on long_periodic_selector
 
-# THIS WON'T WORK, we have to bring in the maxvars_data_statsper which has been period-analyzed, and then do cuts of it that correspond to autovars, etc
-#autovars_true_periodic = ps.periodic_selector(autovars_true)
-#autovars_strict_periodic = ps.periodic_selector(autovars_true)
-
-periodics_s123 = ps.periodic_selector(maxvars_spread_per)
-periodics_s1 = ps.periodic_selector(maxvars_s1_spread_per)
+periodics = ps.best_long_period(
+    ps.long_periodic_selector(maxvars_spread_per, min_period=2, max_period=75), 
+    min_period=2, max_period=75)
 
 maxvars_periodics = maxvars.where( 
-    np.in1d(maxvars.SOURCEID, periodics_s123.SOURCEID) |
-    np.in1d(maxvars.SOURCEID, periodics_s1.SOURCEID) )
-
+    np.in1d(maxvars.SOURCEID, periodics.SOURCEID) )
 
 autovars_true_periodics = autovars_true.where( 
-    np.in1d(autovars_true.SOURCEID, periodics_s123.SOURCEID) |
-    np.in1d(autovars_true.SOURCEID, periodics_s1.SOURCEID) )
+    np.in1d(autovars_true.SOURCEID, periodics.SOURCEID) )
 
 autovars_strict_periodics = autovars_strict.where(
-    np.in1d(autovars_strict.SOURCEID, periodics_s123.SOURCEID) |
-    np.in1d(autovars_strict.SOURCEID, periodics_s1.SOURCEID) )
+    np.in1d(autovars_strict.SOURCEID, periodics.SOURCEID) )
+
 
 c_print( "" )
 c_print( "Number of possible variables with detected periods: %d" % len(maxvars_periodics) )
@@ -272,20 +228,16 @@ c_print( "    %.2f%s, drawn from a looser sample." % (len(autovars_true_periodic
 # The following is only suitable for almost-but-not-quite 
 # accurate histogram analysis (because it ditches the s1-only periodocs)
 
-# intersection of periodics_s123 and maxvars_periodics
-maxvars_periods = periodics_s123.where( 
-    np.in1d(periodics_s123.SOURCEID, maxvars_periodics.SOURCEID))
+# intersection of periodics and maxvars_periodics
+maxvars_periods = periodics.where( 
+    np.in1d(periodics.SOURCEID, maxvars_periodics.SOURCEID))
 
 # etc
-autovars_true_periods = ps.best_period(periodics_s123.where( 
-    np.in1d(periodics_s123.SOURCEID, autovars_true_periodics.SOURCEID)))
+autovars_true_periods = periodics.where( 
+    np.in1d(periodics.SOURCEID, autovars_true_periodics.SOURCEID))
 
-autovars_strict_periods = ps.best_period(periodics_s123.where( 
-    np.in1d(periodics_s123.SOURCEID, autovars_strict_periodics.SOURCEID)))
-
-autovars_true_periods_s1 = ps.best_period(periodics_s1.where( 
-    np.in1d(periodics_s1.SOURCEID, autovars_true_periodics.SOURCEID) &
-    ~np.in1d(periodics_s1.SOURCEID, autovars_true_periods.SOURCEID)))
+autovars_strict_periods = periodics.where( 
+    np.in1d(periodics.SOURCEID, autovars_strict_periodics.SOURCEID))
 
 #print "hey look i'm here"
 
@@ -298,110 +250,3 @@ autovars_true_nonpers = autovars_true.where(
 
 autovars_strict_nonpers = autovars_strict.where(
     ~np.in1d(autovars_strict.SOURCEID, autovars_strict_periodics.SOURCEID))
-
-
-### Here we're gonna sort the NEW subjectives into two categories:
-# Periodic, and Nonperiodic. This will be via a comparison with the 
-# `maxvars_periodics` table.
-
-new_subjectives_nonpers = new_subjectives.where(
-    ~np.in1d(new_subjectives.SOURCEID, maxvars_periodics.SOURCEID))
-
-new_subjectives_per_s123 = periodics_s123.where(
-    np.in1d(periodics_s123.SOURCEID, new_subjectives.SOURCEID))
-
-# those that are in s1 but NOT in s123
-new_subjectives_per_s1 = periodics_s1.where(
-    np.in1d(periodics_s1.SOURCEID, new_subjectives.SOURCEID) & 
-    ~np.in1d(periodics_s1.SOURCEID, periodics_s123.SOURCEID))
-
-#print len(new_subjectives_nonpers)
-#print len(new_subjectives_per_s123)
-#print len(new_subjectives_per_s1)
-#print "those are things. End."
-
-if len(new_subjectives_per_s1) == 0:
-    new_subjectives_per = ps.best_period(new_subjectives_per_s123)
-#    print "assigned best periods"
-
-
-##### Now let's talk about LOW VARIABLES.
-
-# Low variables.
-low_maxvars = sp.where( (sp.Stetson <= 1.0) & (sp.Stetson > 0.55) &(
-        (sp.N_j >= 50) |
-        (sp.N_k >= 50) |
-        (sp.N_h >= 50) ) )
-c_print( "Maximum possible number of LOW-variables: %d" % len(low_maxvars) )
-
-# Low variability criterion has two cases:
-# 1. All 3 bands are quality, and 1.0 > S > 0.55, or
-# 2. 1 or 2 bands is quality and has reduced chisq > 1, and 1 > S > 0.55 still.
-
-# "Quality" is as defined above.
-
-# Constructing these as two separate arrays for ease of reading/editing.
-# Case 1: all 3 bands are quality; S > 1. Note "&"s uniform throughout.
-low_case1 = ( (sp.Stetson > 0.55) & (sp.Stetson <= 1.0) &
-              (sp.pstar_median > 0.75) &
-              (
-        (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
-        (sp.j_mean > 11) & (sp.j_mean < 17) & 
-        (sp.N_j_info == 0) 
-        ) &
-              (
-        (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-        (sp.h_mean > 11) & (sp.h_mean < 16) & 
-        (sp.N_h_info == 0) 
-        ) &
-              (
-        (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
-        (sp.k_mean > 11) & (sp.k_mean < 16) & 
-        (sp.N_k_info == 0)
-        ) )
-
-# Case 2: at least one band quality and rchi^2 > 1; S > 1. Note mixed "&"s 
-# and "|"s, as well as another layer of parentheses around the complex of "|"
-# criteria.
-low_case2 = ( ((sp.Stetson > 0.55) & (sp.Stetson <= 1.0) & 
-               (sp.pstar_median > 0.75)) & (
-        (
-            (sp.N_j >= 50) & (sp.N_j <= 125) &    # J band criteria
-            (sp.j_mean > 11) & (sp.j_mean < 17) & 
-            (sp.N_j_info == 0) & (sp.j_rchi2 > 1) 
-            ) |
-        (
-            (sp.N_h >= 50) & (sp.N_h <= 125) &    # H band criteria
-            (sp.h_mean > 11) & (sp.h_mean < 16) & 
-            (sp.N_h_info == 0) & (sp.h_rchi2 > 1) 
-            ) |
-        (
-            (sp.N_k >= 50) & (sp.N_k <= 125) &    # K band criteria
-            (sp.k_mean > 11) & (sp.k_mean < 16) & 
-            (sp.N_k_info == 0) & (sp.k_rchi2 > 1) 
-            ) ) ) 
-
-low_autovars = sp.where( low_case1 | low_case2 )
-
-low_autovars_strict = sp.where( low_case1 )
-
-
-# Now, to count how many stars have quality that meets "autovars_true".
-# Done above. See "autocan_true" and "autocan_strict".
-
-c_print( "Number of stars automatically classed as LOW variables: %d" % len(low_autovars) )
-c_print( "Number of stars that have the data quality for auto-classification: %d" % len(autocan_true) )
-
-
-low_maxvars_spread = atpy.Table(dropbox_bo_data+"low_maxvars_data_spread.fits")
-low_periodics = ps.best_period(ps.periodic_selector(low_maxvars_spread))
-
-low_strict_periodics = low_periodics.where(
-    np.in1d(low_periodics.SOURCEID, low_autovars_strict.SOURCEID))
-
-low_strict_nonpers = low_autovars_strict.where(
-    ~np.in1d(low_autovars_strict.SOURCEID, low_strict_periodics.SOURCEID))
-
-
-c_print( "Number of LOW strict variables: %d" % len(low_autovars_strict) )
-c_print( "Number of LOW periodic-strict stars: %d" % len(low_strict_periodics))
